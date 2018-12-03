3a4c4de30a42583e2916f507bd8d05beb6c88318
thanhmanci <mahoangtienthanh@gmail.com>
2018-12-03 15:03:04 +0700

    Fix downloadsssssss

--- db/migrate/20181121061652_create_statistics.rb
+++ db/migrate/20181121061652_create_statistics.rb
@@ -3,9 +3,9 @@ class CreateStatistics < ActiveRecord::Migration[5.2]
     create_table :statistics do |t|
       t.integer :material_id, null: false
       t.datetime :time_input
-      t.integer :mass, limit: 18
-      t.integer :price, limit: 18
-      t.integer :total, limit: 18
+      t.integer :mass, limit: 8
+      t.integer :price, limit: 8
+      t.integer :total, limit: 8
       t.timestamps
     end
   end
